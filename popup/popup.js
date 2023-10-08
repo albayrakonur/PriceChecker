d1bed8cb8abbcc1dee422e2a8e358bfd09859b2e
Onur Albayrak <onalbayrakur@gmail.com>
2023-10-08 18:59:29 +0300

    display element value

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,31 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+document.addEventListener("DOMContentLoaded", function () {
+    const getValueBtn = document.getElementById("getValueBtn");
+  
+    getValueBtn.addEventListener("click", function () {
+      alert("sa")
+      const inputId = document.getElementById("inputId").value;
+      const output = document.getElementById("output");
+  
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabs[0].id },
+            function: getValue,
+            args: [inputId],
+          },
+          function (result) {
+            const value = result[0].result;
+            output.innerHTML = `Value of input element with ID "${inputId}": <strong>${value}</strong>`;
+          }
+        );
+      });
+    });
+  });
+  
+  function getValue(inputId) {
+    const inputElement = document.getElementById(inputId);
+    if (inputElement) {
+      return inputElement.value;
+    }
+    return null;
+  }  
